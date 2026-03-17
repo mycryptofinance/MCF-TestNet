5eefded95a8ff62db0305ae9e0a57eb37e9aa4c6
MCF <mycryptofinanceweb3@gmail.com>
2026-03-17 23:22:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -153,8 +153,6 @@ async function connect() {
         fetchMCFData();
         checkNFTs();
         loadRealStakes();
-        tokenContract.balanceOf(userAddress),
-        tokenContract.decimals()
 
         // --- ДОБАВЛЯЕМ ОТРИСОВКУ ТОКЕНОВ ТУТ ---
         if (typeof renderUserTokens === "function") {
